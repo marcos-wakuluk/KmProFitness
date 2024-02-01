ee7c037c7e1c25cb3f6c3003ca0def7123f70976
marcos-wakuluk <wakuluk.marcos@gmail.com>
2024-02-01 15:08:35 -0300

    changes in views and form added for first login

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,15 +4,17 @@ import { useRoute } from '@react-navigation/native';
 
 import AdminView from './AdminView';
 import ClientView from './ClientView';
+import CompletedData from '../screens/CompleteData';
 
 const Home = ({ navigation }) => {
   const route = useRoute();
   const { userRole } = route.params || {};
+  const completedData = false // sacar de user.completedData;
 
   return (
     <View style={styles.container}>
       {userRole === 'admin' && <AdminView navigation={navigation} />}
-      {userRole === 'client' && <ClientView navigation={navigation} />}
+      {userRole === 'client' && completedData ? <ClientView navigation={navigation} /> : <CompletedData />}
     </View>
   );
 };
